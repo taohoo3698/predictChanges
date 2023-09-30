aade8fa2b34dbacb07c5766222b07d2778e44347
Chanatip Ampia <72536538+taohoo3698@users.noreply.github.com>
2023-09-30 16:50:46 +0700

    Update insurance_prediction_app.py

--- insurance_prediction_app.py
+++ insurance_prediction_app.py
@@ -1,33 +1,48 @@
 import streamlit as st
-import pandas as pd
 import joblib
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 # Load the saved model
 loaded_model = joblib.load('linear_regression_model.pkl')
 
+# Define the features for input
+features = ['age', 'bmi', 'children']
+
 # Define the Streamlit app
-st.title('Insurance Charges Prediction')
+st.title("Insurance Charges Prediction")
 
-# Input form for user to enter values
-st.sidebar.header('Enter Your Information')
-age = st.sidebar.slider('Age', 18, 100, 30)
-bmi = st.sidebar.slider('BMI', 15.0, 50.0, 25.0)
-children = st.sidebar.slider('Number of Children', 0, 5, 2)
+# Create a sidebar for user input
+st.sidebar.header('Enter Example Data')
 
-# Create a DataFrame from user input
-input_data = pd.DataFrame({'age': [age], 'bmi': [bmi], 'children': [children]})
+age = st.sidebar.slider("Age", min_value=18, max_value=64, value=30)
+bmi = st.sidebar.slider("BMI", min_value=15, max_value=50, value=25)
+children = st.sidebar.slider("Number of Children", min_value=0, max_value=5, value=2)
 
-# Make predictions when the user clicks the 'Predict' button
-if st.sidebar.button('Predict'):
-    predicted_charges = loaded_model.predict(input_data)
-    st.success(f'Predicted Charges: ${predicted_charges[0]:.2f}')
+# Create a DataFrame with the user input
+example_data = pd.DataFrame([[age, bmi, children]], columns=features)
 
-# Display some information about the model
-st.write('This app uses a linear regression model to predict insurance charges based on your input.')
+# Make predictions
+predicted_charges = loaded_model.predict(example_data)
 
-# Optionally, you can provide additional information about the insurance dataset here.
-# st.write('The model was trained on a dataset of insurance charges.')
+# Display the prediction
+st.sidebar.header('Prediction')
+st.sidebar.write(f"Predicted Charges: ${predicted_charges[0]:.2f}")
 
-# Optionally, provide a link to the source code or dataset.
-# st.write('Source code and dataset: [GitHub Repo](https://github.com/yourusername/your-repo)')
+# Calculate Mean Squared Error (MSE) and Root Mean Squared Error (RMSE)
+actual_charges = 10000  # Replace with the actual charges if available
 
+if actual_charges:
+    mse = mean_squared_error([actual_charges], [predicted_charges[0]])
+    rmse = np.sqrt(mse)
+    st.sidebar.header('Model Evaluation Metrics')
+    st.sidebar.write(f"MSE: {mse:.2f}")
+    st.sidebar.write(f"RMSE: {rmse:.2f}")
+else:
+    st.sidebar.write("Actual charges not provided. Unable to calculate MSE and RMSE.")
+
+# Main content area
+st.header('Insurance Charges Prediction')
+
+# Display additional content or charts in the main area if needed
